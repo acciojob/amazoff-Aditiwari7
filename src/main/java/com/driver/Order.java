8d27bafaa91633bd55440358fdb659ec92220c16
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-01-08 11:29:23 +0530

    try

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -5,12 +5,10 @@ public class Order {
     private String id;
     private int deliveryTime;
 
-    public Order(String id, int deliveryTime) {
+    public Order(String id, String deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
-        this.id = id;
-        this.deliveryTime = deliveryTime;
     }
 
     public String getId() {
